f7265bfa076278a0dcc2619652bc2b9b975fcb57
Ronit Agarwal <ronitagarwal97@gmail.com>
2017-03-16 01:41:10 +0530

    Dream code Projects
    http://www.dreamincode.net/forums/topic/78802-martyr2s-mega-project-ideas-list/

---
+++ exercise1.py
@@ -0,0 +1,11 @@
+#print "Enter String to be Reversed : "
+str = raw_input("")
+
+#for i in range(len(str)-1,0,-1):
+#	print str[i]
+
+#print str[0]
+
+print str[::-1]
+
+	
---
+++ exercise2.py
@@ -0,0 +1,9 @@
+a = raw_input("Enter a Word : ")
+
+b = a[1::]
+c = a[0]
+
+d = "ay"
+
+e = b+c+d
+print e
